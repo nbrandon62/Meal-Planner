bbe256b07d49d2ed627534f89322adba57f314dd
nbrandon62 <nbrandon62@gmail.com>
2022-03-30 17:54:41 -0700

    feature of making menu items bulletpoints on click

--- js/main.js
+++ js/main.js
@@ -7,24 +7,25 @@ let veggieMeals = [
     'hot and sour collards', 'curry cauliflower', 'brown butter carrots ','garlic pea shoots with nut broth ', 'garlic chili eggplant ', 'pineapple fried rice ', 'broccolini and anchovy breacrumbs','coconut and potato soup','black bean chili yuba with lap cheong', 'seared cabbage with tahini caesar', 'mushroom and allium fried rice', 'sashimi salad and soup', 'miso soup, veggies, and salmon', 'chicken caesar salad wraps', 'cobb salad', 'roasted veggie toast', 'chilled sesame noodles', ' veggie crudite with bagna cauda and green tahini', 'green tahini noodles with scallion ginger', 'mushroom tartine', 'bucatini amatraciana', 'spring vegetables ragu and poached egg', 'creamy mushroom pasta', 'kale caesar salad', 'cabbage salad with candied nuts and lemon vin', 'loaded baked potato', 'burrata and pesto foccacia', 'veggie bowls', 'burrata and peas', 'potato breakfast burritos', 'eggplant parm', 'grilled veggies with crunchy garlic', 'salade lyonnaise', ''
 ];
 
-menuList = [];
+const menuList = [];
 
 
 
 //these link the buttons to their respective tasks: Veggie and Protein
-let buttonPro = document.querySelector("#buttonPro");  //protein button
-let buttonVeg = document.querySelector("#buttonVeg");  //veggie button
+let buttonPro = document.querySelector("#buttonPro"); //protein button
+let buttonVeg = document.querySelector("#buttonVeg"); //veggie button
 let answerPro = document.querySelector("#answerPro"); //protein answer box
 let answerVeg = document.querySelector("#answerVeg"); // veggie answer box
 
-let addProBtn = document.querySelector("#menuBtnPro");
-let addVegBtn = document.querySelector("#menuBtnVeg");
-let menu = document.querySelector("#menuText");
+let addProBtn = document.querySelector("#menuBtnPro"); // add protein to menu button
+let addVegBtn = document.querySelector("#menuBtnVeg"); // add veggie to menu button
+let menu =document.querySelector("#menuText"); // weekly menu list 
 
 
 //EVENT LISTENERS -------------------------------------------------
 buttonPro.addEventListener('click', () => {
     pickDish("protein")
+    console.log(pickDish);
 })
 
 buttonVeg.addEventListener('click', () => {
@@ -33,7 +34,7 @@ buttonVeg.addEventListener('click', () => {
 
 
 addProBtn.addEventListener('click', () => {
-    addToList("protein")
+    addToList("protein");
 })
 
 addVegBtn.addEventListener('click', () => {
@@ -71,11 +72,32 @@ function addToList(dish){
     let addVeggie = answerVeg.innerHTML
 
     if (dish ==="protein"){
-        addToMenu(addProtein)
-        menuText.innerHTML = menuList
+        addToMenu(addProtein);
+        makeList();
+        // menuText.innerHTML = menuList
     }else if (dish === "veggie") {
         addToMenu(addVeggie)
-        menuText.innerHTML = menuList
+        // menuText.innerHTML = menuList
+        makeList();
 
     }
+}
+
+function makeList() { 
+    // Make a container element for the list
+    let listContainer = document.createElement('div');
+    // Make the list
+    let listElement = document.createElement('ul');
+    // create an item for each one
+    let listItem = document.createElement('li');
+
+    // Add it to the page
+    document.getElementsByClassName('col-md-8')[0].appendChild(listContainer);
+    listContainer.appendChild(listElement);
+
+    // Add the last item in the array 
+    listItem.innerHTML = menuList[menuList.length - 1];
+
+    // Add listItem to the listElement
+    listElement.appendChild(listItem);
 }

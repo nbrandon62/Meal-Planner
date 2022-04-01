ca8f2fb2b00714f6b4c1d10853de453b4a061276
nbrandon62 <nbrandon62@gmail.com>
2022-04-01 15:59:48 -0700

    feat added a clear menu button and styled it. Added more dishes

--- js/main.js
+++ js/main.js
@@ -1,14 +1,13 @@
 // ARRAYS --------------------------------------------------------
 let proteinMeals = [
-    'chicken shawarma bowl', 'miso chicken', 'cumin lamb', 'chicken fried chicken', 'harissa chicken bowl', 'pad see ew', 'beef rice rolls', 'veggie/ raw fish handrolls', 'banh xeo', 'fish with shirodashi butter', 'fish with beurre blanc', 'butter chicken', 'chicken bacon ranch sammy', 'teri chicken', 'teri beef', 'meat jun', 'steak sandwich', 'shortrib locomoco', 'garlic shrimp', '40 clove chicken', 'teriyaki salmon', 'Vietnamese fried fish', 'chicken piccata', 'ginger steamed fish', 'kalbijim', 'japanese curry', 'dijon braised chicken legs', 'chicken fricasee', 'grilled spicy chicken sandwich', 'pork katsu and cabbage', 'hainan chicken rice', 'thit kho', 'massaman curry', 'brown butter chicken breast', 'meatloaf and cheesy rice', 'chicken alfredo', 'jerk chicken', 'croque monsieur', 
+    'chicken shawarma bowl', 'miso chicken', 'cumin lamb', 'chicken fried chicken', 'harissa chicken bowl', 'pad see ew', 'beef rice rolls', 'veggie/ raw fish handrolls', 'banh xeo', 'fish with shirodashi butter', 'fish with beurre blanc', 'butter chicken', 'chicken bacon ranch sammy', 'teri chicken', 'teri beef', 'meat jun', 'steak sandwich', 'shortrib locomoco', 'garlic shrimp', '40 clove chicken', 'teriyaki salmon', 'Vietnamese fried fish', 'chicken piccata', 'ginger steamed fish', 'kalbijim', 'japanese curry', 'dijon braised chicken legs', 'chicken fricasee', 'grilled spicy chicken sandwich', 'pork katsu and cabbage', 'hainan chicken rice', 'thit kho', 'massaman curry', 'brown butter chicken breast', 'meatloaf and cheesy rice', 'chicken alfredo', 'jerk chicken', 'croque monsieur', 'steak and potatoes', 'basil fish', 'black bean fish', 'salt & pepper fish', 'lap cheong sticky rice', 'peas and bacon pasta', 'roast chicken', 'pesto salmon', 'cheeseburgers', 'lion\'s head meatball soup', 'char siu pork ribs', 'late night nachos', 'shaking beef', 'khao soi',
 ];
 
 let veggieMeals = [
-    'hot and sour collards', 'curry cauliflower', 'brown butter carrots ','garlic pea shoots with nut broth ', 'garlic chili eggplant ', 'pineapple fried rice ', 'broccolini and anchovy breadcrumbs','coconut and potato soup','black bean chili yuba with lap cheong', 'seared cabbage with tahini caesar', 'mushroom and allium fried rice', 'sashimi salad and soup', 'miso soup, veggies, and salmon', 'chicken caesar salad wraps', 'cobb salad', 'roasted veggie toast', 'chilled sesame noodles', ' veggie crudite with bagna cauda and green tahini', 'green tahini noodles with scallion ginger', 'mushroom tartine', 'bucatini amatraciana', 'spring vegetables ragu and poached egg', 'creamy mushroom pasta', 'kale caesar salad', 'cabbage salad with candied nuts and lemon vin', 'loaded baked potato', 'burrata and pesto foccacia', 'veggie bowls', 'burrata and peas', 'potato breakfast burritos', 'eggplant parm', 'grilled veggies with crunchy garlic', 'salade lyonnaise',
+    'hot and sour collards', 'curry cauliflower', 'brown butter carrots ','garlic pea shoots with nut broth ', 'garlic chili eggplant ', 'pineapple fried rice ', 'broccolini and anchovy breadcrumbs','coconut and potato soup','black bean chili yuba with lap cheong', 'seared cabbage with tahini caesar', 'mushroom and allium fried rice', 'sashimi salad and soup', 'miso soup, veggies, and salmon', 'chicken caesar salad wraps', 'cobb salad', 'roasted veggie toast', 'chilled sesame noodles', ' veggie crudite with bagna cauda and green tahini', 'green tahini noodles with scallion ginger', 'mushroom tartine', 'bucatini amatraciana', 'spring vegetables ragu and poached egg', 'creamy mushroom pasta', 'kale caesar salad', 'cabbage salad with candied nuts and lemon vin', 'loaded baked potato', 'burrata and pesto foccacia', 'veggie bowls', 'burrata and peas', 'potato breakfast burritos', 'eggplant parm', 'grilled veggies with crunchy garlic', 'salade lyonnaise', 'greens and oyster sauce', 'coconut vegetable soup', 'black bean chili eggplant', 'jap chae noodles', 'grilled broccoli and romesco', 'bok choy and dried shrimp'
 ];
 
 const menuList = [];
-
 
 
 //these link the buttons to their respective tasks: Veggie and Protein
@@ -20,6 +19,7 @@ let answerVeg = document.querySelector("#answerVeg"); // veggie answer box
 let addProBtn = document.querySelector("#menuBtnPro"); // add protein to menu button
 let addVegBtn = document.querySelector("#menuBtnVeg"); // add veggie to menu button
 let menu = document.querySelector("#menuText"); // weekly menu list 
+let menuClear = document.querySelector("#menuClear");
 
 
 //EVENT LISTENERS -------------------------------------------------
@@ -40,6 +40,9 @@ addVegBtn.addEventListener('click', () => {
     addToList("veggie")
 })
 
+menuClear.addEventListener('click', () => {
+    empty();
+})
 
 //FUNCTIONS---------------------------------------------------------
 function pickRandomMeal (mealArray) { //inputs Array of Strings
@@ -97,4 +100,8 @@ function makeList() {
 
     // Add listItem to the listElement
     listElement.appendChild(listItem);
+}
+
+function empty() {
+    document.getElementById("menuText").innerHTML = "";
 }
